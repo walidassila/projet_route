04500954d5e95fcb4020da7e358dffaed1087d68
walid <walidassila06@gmail.com>
2025-08-09 21:43:17 +0200

    done

--- trait_video.py
+++ trait_video.py
@@ -4,7 +4,6 @@ from video_utils import prepare_video
 from bounding_boxes import draw_boxes
 import ultralytics
 import os
-from tracker_utils import  yolo_to_bytetrack_detections,create_tracker
 from labels_utils import remplace_name
 
 
@@ -32,7 +31,7 @@ def trait_video(model,input_path,output_folder=None,conf=0.4,class_names=None):
     video_out.release()
     print("✅ Traitement terminé, vidéo sauvegardée dans :", output_path)
 
-def trait_tracking(model,input_path,output_folder=None,conf=0.4,class_colors=None):
+a="""def trait_tracking(model,input_path,output_folder=None,conf=0.4,class_colors=None):
     if output_folder is None:
         output_folder=os.getcwd()
     
@@ -45,4 +44,4 @@ def trait_tracking(model,input_path,output_folder=None,conf=0.4,class_colors=Non
         results=model.predict(frame,imgsz=640,conf=conf,verbose=False)[0]
         frame_shape = frame.shape[:2]
         detections = yolo_to_bytetrack_detections(results)
-        online_targets = tracker.update(detections, frame_shape, frame_shape)
+        online_targets = tracker.update(detections, frame_shape, frame_shape)"""
